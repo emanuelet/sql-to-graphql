94038b72cb19f7b4ab8e6c443e346e4dd291536d
Etienne Bouchard <bouchaet@gmail.com>
2017-11-21 14:11:11 -0500

    workable knex select for column info

--- backends/mssqltest.js
+++ backends/mssqltest.js
@@ -11,14 +11,40 @@ var knex = require('knex')({
   }
 })
 
-knex('SalesLT.Customer')
-  .innerJoin('SalesLT.CustomerAddress', 'SalesLT.Customer.CustomerID', 'SalesLT.CustomerAddress.CustomerID')
-  .innerJoin('SalesLT.Address', 'SalesLT.CustomerAddress.AddressID', 'SalesLT.Address.AddressID')
-  .whereRaw('SalesLT.Customer.FirstName LIKE ?', ['O%'])
-  .limit(2)
-  .then(function (rows) {
+// knex('SalesLT.Customer')
+//   .innerJoin('SalesLT.CustomerAddress', 'SalesLT.Customer.CustomerID', 'SalesLT.CustomerAddress.CustomerID')
+//   .innerJoin('SalesLT.Address', 'SalesLT.CustomerAddress.AddressID', 'SalesLT.Address.AddressID')
+//   .whereRaw('SalesLT.Customer.FirstName LIKE ?', ['O%'])
+//   .limit(2)
+//   .then(function (rows) {
+//     console.log(rows)
+//   })
+//   .finally(function () {
+//     return knex.destroy()
+//   })
+
+knex
+  .column([
+    {column_name: 'columns.name'}, 
+    {ordinal_position: 'columns.column_id'},
+    {data_type: 'types.name'},
+    'columns.is_nullable',
+    knex.raw('CASE WHEN indexes.is_primary_key = 1 THEN \'PRI\' ELSE \'\' END as column_primary')])
+  .select()
+  .from('sys.columns')
+  .innerJoin('sys.types', 'sys.columns.user_type_id', 'sys.types.user_type_id')
+  .innerJoin('sys.objects', 'sys.columns.object_id', 'sys.objects.object_id')
+  .leftOuterJoin('sys.index_columns', function() {
+    this.on('sys.columns.object_id', 'sys.index_columns.object_id')
+       .andOn('sys.columns.column_id', 'sys.index_columns.column_id')})
+  .leftOuterJoin('sys.indexes', function() {
+    this.on('sys.index_columns.object_id', 'sys.indexes.object_id')
+        .andOn('sys.index_columns.index_id','sys.indexes.index_id')})
+  .whereRaw('columns.object_id = object_id(schema_name(objects.schema_id) + ?)', ['.product'])
+  .orderBy('columns.column_id')
+  .then(function(rows) {
     console.log(rows)
   })
-  .finally(function () {
+  .finally(function() {
     return knex.destroy()
-  })
+  });
---
+++ backends/mssqltest.sql
@@ -0,0 +1,23 @@
+
+SELECT 
+    c.name 'column_name',
+    t.Name 'data_type',
+	c.column_id,
+    c.max_length 'max_length',
+    c.precision ,
+    c.scale ,
+    c.is_nullable,
+    i.is_primary_key
+FROM    
+    sys.columns c
+INNER JOIN 
+    sys.types t ON c.user_type_id = t.user_type_id
+INNER JOIN
+	sys.objects o ON o.object_id = c.object_id
+LEFT OUTER JOIN 
+    sys.index_columns ic ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+LEFT OUTER JOIN 
+    sys.indexes i ON ic.object_id = i.object_id AND ic.index_id = i.index_id
+WHERE
+    c.object_id = OBJECT_ID(schema_name(o.schema_id) + '.ViewAuthorizedPublicMarketIASTransactions_v2')
+ORDER BY c.column_id
